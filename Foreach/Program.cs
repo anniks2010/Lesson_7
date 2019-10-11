7bf4bcf8ef7713e342db54269f9b9cccbd04af33
Opilane TTHK <opilane@tthk.ee>
2019-10-11 18:18:18 +0300

    Lesson 7

---
+++ Foreach/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //to find the length of a string foreach on nagu otsing, otsime ühte sõna mingi hulga seest.
+            /*Console.WriteLine("Please enter string: ");
+            string userString = Console.ReadLine();
+
+            int counter = 0;
+
+            foreach (char letter in userString)
+            {
+                counter++;
+            }
+
+            Console.WriteLine($"Your name is {counter} letters long.");
+            Console.ReadLine();*/
+
+            /// Teine võimalus foreach kasutada:  
+            Console.WriteLine("Please enter your name: ");
+            string userName = Console.ReadLine().ToLower();
+            bool letterFound = false;
+
+            foreach (char letter in userName)
+            {
+                if (letter == 'a')
+                {
+                    letterFound = true;
+                    break;
+                }
+
+            }
+
+            string result = letterFound ? "a täht on olemas" : "a täht ei ole olemas"; ///seda saab kasutada, kui on kaks valikut.
+            Console.WriteLine(result);
+
+            /*if (letterFound) 
+            {
+                Console.WriteLine("'a' täht on olemas sõnas");
+            }
+            else
+            {
+                Console.WriteLine("'a' täht ei ole sõnas olemas");
+            }*/
+
+            Console.ReadLine();
+
+
+            //To separate the individual characters from a string
+            /*Console.WriteLine("Please enter word: ");
+            string userWord = Console.ReadLine();
+
+            foreach (char letter in userWord)
+            {
+                Console.Write($"{letter} ");
+            }
+
+
+
+            Console.ReadLine();*/
+
+
+
+
+
+
+        }
+    }
+}
--- Lesson_7/Program.cs
+++ Lesson_7/Program.cs
@@ -10,6 +10,88 @@ namespace Lesson_7
     {
         static void Main(string[] args)
         {
+
+            //Korrutustabel:
+            /*Console.WriteLine("Insert a number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+           
+            for (int i= 1; i <= 10; i++)
+            {
+                
+                Console.WriteLine($"{userNumber} x {i} = {userNumber*i}");
+            }
+
+            Console.ReadLine();*/
+
+
+            /*Console.WriteLine("Insert word: ");
+            string userWord = Console.ReadLine().ToLower();
+            
+            int aTotal = 0;
+           
+
+            for (int i = 0; i< userWord.Length; i++)
+            {
+                if (userWord[i]== 'a')
+                {
+                    aTotal++;
+                }
+            }
+
+            Console.WriteLine($"There are {aTotal} \"A\" letters in your name.");
+            Console.ReadLine();*/
+
+
+            //To separate the individual characters from a string
+
+            /*Console.Write("Please enter word: ");
+            string userWord = Console.ReadLine();
+
+
+
+           for (int i = 0; i< userWord.Length; i++)
+            {
+                if (i == userWord.Length-1)
+                {
+                    Console.Write($"{userWord[i]}#");
+                    break;
+                }
+                else
+                {
+                    Console.Write($"{userWord[i]}*");
+                }
+                     
+               
+
+            }
+            Console.ReadLine();*/
+
+            /* int i = 1;
+
+             while (i <= 10)
+             {
+                 Console.WriteLine(i);
+                 i++;
+             }
+             Console.ReadLine();*/
+
+            string hello = "hello";
+            int i = 0;
+
+            while (i< hello.Length)
+            {
+                if (i== hello.Length - 1)
+                {
+                    Console.Write($"{hello[i]}");
+                    break;
+                }
+            
+                Console.Write($"{hello[i]}*");
+                i++;
+            }
+            Console.ReadLine();
+
+
         }
     }
 }
